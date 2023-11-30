5f6f4abf60f68911b3081da8b07116784f864afd
Ayush <86900579+Southpaw09@users.noreply.github.com>
2023-11-30 14:45:34 +0000

    DataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all JS code as newer version
+
+
+// alert(3+3); "We are using node js and not browser so it will give error"
+
+//code readability should be good
+
+let name = "Ayush";
+let age = 18;
+let isLoggedIn = false;
+
+//number
+//bigInt
+//string
+//boolean
+//null => its a type as well as value e.g. = Temperature is null means value is not there yet //typeof => object
+//undefined => variable is declared but not assigned //typeof => undefined
+//symbol => unique
+
+
+//object
+
+
+console.log(typeof(null));
