b32faf94cd0658691c7a6a5154d2d4b3df681305
Ayush <86900579+Southpaw09@users.noreply.github.com>
2023-11-30 14:31:56 +0000

    Variables Done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -8,6 +8,19 @@ let accountState;//undefined
 Prefer not to use var due to scoping reasons.
 */
 
+/*
+Hoisting is JavaScript's default behavior of moving all declarations to the top of the current scope 
+(to the top of the current script or the current function).
+*/
+
+/*
+KEYWORD	                   SCOPE	             REDECLARATION & REASSIGNMENT	                HOISTING
+var                 	Global, Local	                yes & yes	                    yes, with default value
+let	                Global, Local, Block	            no & yes	                    yes, without default value
+const	            Global, Local, Block	            no & no	                        yes, without default value
+*/
+
+
 //problemState is not defined and accountState is undefined
 
 // accountId = 2; this is not valid
